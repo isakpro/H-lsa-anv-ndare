d9958b9a7352ef400c3cf59d1e35673f0b399a65
isakr <isakr@DESKTOP-QENPR6F>
2025-09-15 09:32:53 +0200

    La till välkommen till programmet

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hälsa_användare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine();
+            Console.WriteLine("Hej " + namn + ", Välkommen till programmet!");
+            Console.ReadLine();
+        }
+
+
+
+
+
+    }
+}
